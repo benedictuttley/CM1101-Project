b28cce6df6850be2463104ef4ca6f86f1b1c2f8e
RedPandaDev <katukasz@gmail.com>
2016-10-13 16:22:12 +0100

    added basic stats

--- enemy.py
+++ enemy.py
@@ -1,1 +1,2 @@
 #This has enemy information and stats
+enemy_stats = {"strength": 2,"defence": 2, "health": 50, "alive": True}
--- player.py
+++ player.py
@@ -1,1 +1,2 @@
 #This has the player information and stats
+player_stats = {"strength": 5,"defence": 5, "health": 100, "alive": True}
