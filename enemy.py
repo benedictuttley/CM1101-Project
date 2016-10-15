72144b1e3d329277f06404368641bb33ef4bac21
RedPandaDev <katukasz@gmail.com>
2016-10-15 21:15:48 +0100

    Added different enemies
    Basic enemies with stats and descriptions.

--- enemy.py
+++ enemy.py
@@ -1,2 +1,42 @@
-#This has enemy information and stats
-enemy_stats = {"strength": 2,"defence": 2, "health": 50, "alive": True}
+# use if enemy_stats == None to check if it's empty
+enemy_stats = {}
+
+enemy_pirate = {
+"strength": 2,
+"defence": 10,
+"health": 100, 
+"alive": True, 
+"attack":(5,10),
+"name":"Space Pirate", 
+"description":"""You encountered a Space Pirate.
+
+From your knowledge, Space Pirates have a lot of health 
+as they don't have many real body parts since most were replaced 
+because of previous injuries. However they don't hit hard."""
+}
+enemy_alien = {
+"strength": 10,
+"defence": 2,
+"health": 25, 
+"alive": True, 
+"attack":(20,30), 
+"name":"Alien",
+"description":"""You encountered an Alien.
+
+It looks weird and hits like a space shuttle.
+Aliens can kill some people with one hit, 
+however they don't have much health"""
+}
+enemy_monster = {
+"strength": 5,
+"defence": 5,
+"health": 50, 
+"alive": True, 
+"attack":(10,20), 
+"name":"Goopy Monster",
+"description":"""You encountered a Goopy Monster.
+ 
+Not a lot is know about them or where they came from,
+most likely a failed lab experiment.
+Goopy Monsters have a nice mix of health and strength"""
+}
