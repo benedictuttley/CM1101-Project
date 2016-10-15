bf57cc42de5f84bb718520d3deae33c568c07d73
RedPandaDev <katukasz@gmail.com>
2016-10-15 21:33:03 +0100

    Added exp and level
    Plus a couple of wepons to work with combat etc.

--- player.py
+++ player.py
@@ -1,2 +1,3 @@
 #This has the player information and stats
-player_stats = {"strength": 5,"defence": 5, "health": 100, "alive": True}
+player_stats = {"strength": 5,"defence": 5, "health": 100, "alive": True, "weapon":None, "experience": 0, "level": 1}
+weapons = {"stick": (10, 15), None:(1,10), "knife":(10,20), "gun":(30,40)}
