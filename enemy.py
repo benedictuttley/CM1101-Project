bb5016de8365b076e136fb659783c9916e2ff90c
Hawkzed <hawkzed@hotmail.co.uk>
2016-10-17 16:41:48 +0100

    Added extra enemies to the pirate faction.
    Also updated descriptions for the sake of lore and immersion.

--- enemy.py
+++ enemy.py
@@ -14,6 +14,93 @@ From your knowledge, Space Pirates have a lot of health
 as they don't have many real body parts since most were replaced 
 because of previous injuries. However they don't hit hard."""
 }
+
+enemy_pirate_corsair = {
+"strength": 2,
+"defence": 10,
+"health": 100, 
+"alive": True, 
+"attack":(5,10),
+"name":"Pirate Corsair", 
+"description":"""You encountered a Space Pirate Corsair.
+The weakest known space pirates to actually get involved with a boarding action.
+They serve as meat shields and labour for the more powerful pirates 
+and are often looking for the perfect time to stab their superior in the back 
+and earn a promotion. 
+
+From your knowledge, Space Pirates have a lot of health 
+as they have replaced large portions of their bodies with crude 
+cybernetics after grevious injuries. However they don't hit hard 
+due to the poor quality of the cyberlimbs."""
+}
+
+enemy_pirate_armsman = {
+"strength": 2,
+"defence": 10,
+"health": 100, 
+"alive": True, 
+"attack":(5,10),
+"name":"Pirate Armsman", 
+"description":"""You encountered a Space Pirate Armsman.
+Their curious name comes from the heavy weapon systems that 
+replace their previously human arms in an attempt to bring 
+anti-ship weaponry onto a more portable platform. 
+Most Armsmen underwent sugery without their consent and 
+can only take their rage out on their victims, or you for that matter.
+
+
+From your knowledge, Space Pirates have a lot of health 
+as they have replaced large portions of their bodies with crude 
+cybernetics after grevious injuries. However they don't hit hard 
+due to the poor quality of the cyberlimbs."""
+}
+
+enemy_pirate_ravager = {
+"strength": 2,
+"defence": 10,
+"health": 100, 
+"alive": True, 
+"attack":(5,10),
+"name":"Pirate Ravager", 
+"description":"""You encountered a Pirate Ravager.
+
+Commonly used as walking breaching tools and salvagers 
+these pirates are a common site on raided stations where their 
+vast arsenal of tools assist pirate boarding operations. 
+Ravagers are walking Swiss Army Knives with most of their 
+upper torso replaced with a multitude of industrial tools 
+and vicious close range weapons. 
+
+From your knowledge, Space Pirates have a lot of health 
+as they have replaced large portions of their bodies with crude 
+cybernetics after grevious injuries. However they don't hit hard 
+due to the poor quality of the cyberlimbs."""
+}
+
+enemy_pirate_lord = {
+"strength": 2,
+"defence": 10,
+"health": 100, 
+"alive": True, 
+"attack":(5,10),
+"name":"Pirate Lord", 
+"description":"""You encountered a Space Pirate Lord.
+
+Leading from the front has always been the best method of 
+insuring loyalty among pirate factions and this Pirate Lord 
+is demonstrating exactly that. He stands before you to make 
+an example out of you, to demonstrate to his subordinates 
+that no one stands in the way of a pirate legion
+
+This fight is no joke. Standing in front of you is an assailant 
+of immense fortitude and power. His immense stature augemented with cybertechnology 
+far more powerful than you have encountered on any of his whelps before. 
+His mere presence causes cold sweat to pour down your face and his voice 
+booms like a thousand drums beating in unison. This will not be an easy 
+fight, feel free to retreat and come back when you are better prepared to 
+deal with such a fearsome foe."""
+
+}
 enemy_alien = {
 "strength": 10,
 "defence": 2,
